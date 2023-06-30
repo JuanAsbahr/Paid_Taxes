76f669d7da11005f84353eb82f2633639e6ad585
JuanAsbahr <juan_asbahr@hotmail.com>
2023-06-30 11:31:47 -0300

    Program Ok

--- Program.cs
+++ Program.cs
@@ -15,15 +15,15 @@ internal class Program
         {
             Console.WriteLine($"Tax payer #{i} data");
             Console.Write("Individual or company (i/c)? ");
-            char ch = char.Parse(Console.ReadLine());
+            char type = char.Parse(Console.ReadLine());
             Console.Write("Name: ");
             string name = Console.ReadLine();
             Console.Write("Anual Income: $ ");
             double anualIncome = double.Parse(Console.ReadLine());
 
-            if (ch == i)
+            if (type == 'i')
             {
-                Console.Write("Health expenditures: $");
+                Console.Write("Health expenditures: $ ");
                 double healthExpenditures = double.Parse(Console.ReadLine());
                 list.Add(new Individual(name, anualIncome, healthExpenditures));
             }
