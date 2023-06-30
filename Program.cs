6735342b79b619efb00c55144944c323183e6e29
JuanAsbahr <juan_asbahr@hotmail.com>
2023-06-30 11:25:19 -0300

    Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Paid_Taxes.Entities;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<TaxPayer> list = new List<TaxPayer>();
+        
+        Console.Write("Enter the number of tax payers: ");
+        int numberTax = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        for (int i = 1; i <= numberTax; i++)
+        {
+            Console.WriteLine($"Tax payer #{i} data");
+            Console.Write("Individual or company (i/c)? ");
+            char ch = char.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Anual Income: $ ");
+            double anualIncome = double.Parse(Console.ReadLine());
+
+            if (ch == i)
+            {
+                Console.Write("Health expenditures: $");
+                double healthExpenditures = double.Parse(Console.ReadLine());
+                list.Add(new Individual(name, anualIncome, healthExpenditures));
+            }
+            else
+            {
+                Console.Write("Number of employees: ");
+                int numberOfEmployees = int.Parse(Console.ReadLine());
+                list.Add(new Company(name, anualIncome, numberOfEmployees));
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("TAXES PAID:");
+        foreach (TaxPayer taxPayer in list)
+        {
+            Console.WriteLine(taxPayer.Tax().ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+
+    }
+}
